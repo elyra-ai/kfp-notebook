6d4c4af574ca4b5aec9abbe98ad258c946bc1027
Luciano Resende <lresende@us.ibm.com>
2019-08-15 11:17:13 -0700

    Use publicly available url for bootstrapper

--- notebook/pipeline/_notebook_op.py
+++ notebook/pipeline/_notebook_op.py
@@ -70,9 +70,11 @@ class NotebookOp(ContainerOp):
             if 'bootstrap_script' not in kwargs:
                 """ If bootstrap_script arg with URL not provided, use the one baked in here.
                 """
-                self.bootstrap_script_url = 'https://raw.github.ibm.com/ai-workspace/kfp-notebook/' \
-                                            'master/etc/docker-scripts/' \
-                                            'bootstrapper.py?token=AAAcKx3m6ZFVbMNDYJCoG0DHhiWL-D_Jks5dXuAKwA%3D%3D'
+                # self.bootstrap_script_url = 'https://raw.github.ibm.com/ai-workspace/kfp-notebook/' \
+                #                             'master/etc/docker-scripts/' \
+                #                             'bootstrapper.py?token=AAAcK3n713Mj5tLZMsD7c3Pmc0kJAZ4Yks5dVEU_wA%3D%3D'
+
+                self.bootstrap_script_url = 'http://www.figueiredos.com/workspace/bootstrapper.py'
             else:
                 self.bootstrap_script_url = kwargs['bootstrap_script']
 
