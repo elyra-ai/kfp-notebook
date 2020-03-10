df0bc86a37a9930b82ab13493aa824c698bb55d8
Luciano Resende <lresende@us.ibm.com>
2020-03-10 00:13:30 -0700

    KFP Notebook release 0.7.0

--- notebook/__init__.py
+++ notebook/__init__.py
@@ -15,4 +15,4 @@
 # limitations under the License.
 #
 
-__version__ = '0.7.0.dev0'
+__version__ = '0.7.0'
--- setup.py
+++ setup.py
@@ -63,6 +63,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/elyra/kfp-notebook',
-    version='0.7.0.dev0',
+    version='0.7.0',
     zip_safe=False,
 )
