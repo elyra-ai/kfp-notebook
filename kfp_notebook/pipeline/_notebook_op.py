04e5dd55ee373cab553cfc7f773370c368f6e257
Luciano Resende <lresende@us.ibm.com>
2020-11-09 17:25:50 -0800

    Update kfp_notebook/pipeline/_notebook_op.py
    Co-authored-by: Kevin Bates <kbates4@gmail.com>

--- kfp_notebook/pipeline/_notebook_op.py
+++ kfp_notebook/pipeline/_notebook_op.py
@@ -71,7 +71,7 @@ class NotebookOp(ContainerOp):
         """
 
         self.notebook = notebook
-        self.notebook_name = self._get_file_name_with_extension(notebook)
+        self.notebook_name = os.path.basename(notebook)
         self.cos_endpoint = cos_endpoint
         self.cos_bucket = cos_bucket
         self.cos_directory = cos_directory
