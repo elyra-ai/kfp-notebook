83aa55152433d9894dae374e012dfc685a6b1521
Luciano Resende <lresende@us.ibm.com>
2021-03-17 16:38:01 -0700

    Prepare for next development iteration

--- setup.py
+++ setup.py
@@ -61,6 +61,6 @@ setup(
     test_suite='tests',
     tests_require=test_requirements,
     url='https://github.com/elyra-ai/kfp-notebook',
-    version='0.21.0',
+    version='0.22.0.dev0',
     zip_safe=False,
 )
